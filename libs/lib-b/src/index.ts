8780144d5b523d8deda78d79f97b7d02c8a4f84a
Devin Fee <devin@devinfee.com>
2020-09-12 02:52:56 -0700

    added change for wrapping text in lib-b

--- libs/lib-b/src/index.ts
+++ libs/lib-b/src/index.ts
@@ -3,5 +3,5 @@
  * @param s the text that will be wrapped
  * @param wrap the characters that will wrap the text
  */
-export const wrapString = (s: string, wrap = "*"): string =>
+export const wrapString = (s: string, wrap = "~"): string =>
   `${wrap}${s}${wrap}`;
