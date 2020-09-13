aaab5da4eba056b27ec8cd38296036484f37b6cf
Devin Fee <devin@devinfee.com>
2020-09-12 23:56:02 -0700

    removed eslint-plugin-json-format from eslint configs

--- rigs/eslint-config/src/node.ts
+++ rigs/eslint-config/src/node.ts
@@ -2,7 +2,6 @@ import { baseConfig } from "./rules/base-config";
 import { commentsConfig } from "./rules/comments-config";
 import { importConfig } from "./rules/import-config";
 import { jestConfig } from "./rules/jest-config";
-import { jsonFormatConfig } from "./rules/json-format";
 import { prettierConfig } from "./rules/prettier-config";
 import { promiseConfig } from "./rules/promise-config";
 import { typescriptConfig } from "./rules/typescript-config";
@@ -26,16 +25,11 @@ export = typedLinterConfig({
   globals: {
     process: true,
   },
-  ignorePatterns: [
-    ...baseConfig.ignorePatterns,
-    ...jestConfig.ignorePatterns,
-    ...jsonFormatConfig.ignorePatterns,
-  ],
+  ignorePatterns: [...baseConfig.ignorePatterns, ...jestConfig.ignorePatterns],
   overrides: [...commentsConfig.overrides, ...jestConfig.overrides],
   parser: typescriptConfig.parser,
   parserOptions: typescriptConfig.parserOptions,
   plugins: [
-    ...jsonFormatConfig.plugins,
     ...prettierConfig.plugins,
     ...promiseConfig.plugins,
     ...typescriptConfig.plugins,
--- rigs/eslint-config/src/react.ts
+++ rigs/eslint-config/src/react.ts
@@ -2,7 +2,6 @@ import { baseConfig } from "./rules/base-config";
 import { commentsConfig } from "./rules/comments-config";
 import { importConfig } from "./rules/import-config";
 import { jestConfig } from "./rules/jest-config";
-import { jsonFormatConfig } from "./rules/json-format";
 import { prettierConfig } from "./rules/prettier-config";
 import { promiseConfig } from "./rules/promise-config";
 import { reactConfig } from "./rules/react-config";
@@ -19,16 +18,11 @@ export = typedLinterConfig({
     ...prettierConfig.extends,
     ...reactConfig.extends,
   ],
-  ignorePatterns: [
-    ...baseConfig.ignorePatterns,
-    ...jestConfig.ignorePatterns,
-    ...jsonFormatConfig.ignorePatterns,
-  ],
+  ignorePatterns: [...baseConfig.ignorePatterns, ...jestConfig.ignorePatterns],
   overrides: [...commentsConfig.overrides, ...jestConfig.overrides],
   parser: typescriptConfig.parser,
   parserOptions: typescriptConfig.parserOptions,
   plugins: [
-    ...jsonFormatConfig.plugins,
     ...prettierConfig.plugins,
     ...promiseConfig.plugins,
     ...reactConfig.plugins,
